2e47ed279f34a80fe12e4ba427844e4c3269f0b1
Mubin Ansari <mubinansari910@gmail.com>
2023-03-12 01:37:26 +0530

    add signout button to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { useAuthState, useSignOut } from 'react-firebase-hooks/auth';
+import { auth } from '../firebase';
 
 export default function Home() {
-  return <div>Home</div>;
+  const [signOut] = useSignOut(auth);
+  const [user] = useAuthState(auth);
+  return (
+    <div className='main'>
+      <h1>Hello, {user.displayName}</h1>
+      <button onClick={() => signOut()} className='primary-button'>
+        Sign out
+      </button>
+    </div>
+  );
 }
