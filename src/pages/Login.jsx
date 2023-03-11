3714c5c4852e4c76f846073868bdb0b6fe18e642
Mubin Ansari <mubinansari910@gmail.com>
2023-03-12 01:26:23 +0530

    add checks to Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useAuthState, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { Navigate } from 'react-router';
 import { auth } from '../firebase';
 
 export default function Login() {
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [signInWithGoogle, loading, error] = useSignInWithGoogle(auth);
+
+  const [user] = useAuthState(auth);
+
+  if (error) {
+    console.error(error);
+  }
+
+  if (user) {
+    return <Navigate to='/' />;
+  }
 
   return (
     <div className='main'>
-      <h1 className='title'>
-        Welcome to <span className='text-lemon'>Converse</span> 👋
-      </h1>
-      <p className='mb-2'>Want to get started?</p>
-      <button onClick={() => signInWithGoogle()} className='primary-button'>
-        Sign in with Google
-      </button>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <h1 className='title'>
+            Welcome to <span className='text-lemon'>Converse</span> 👋
+          </h1>
+          <p className='mb-2'>Want to get started?</p>
+          <button onClick={() => signInWithGoogle()} className='primary-button'>
+            Sign in with Google
+          </button>
+        </>
+      )}
     </div>
   );
 }
