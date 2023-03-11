0ac359e952d9fb9178da8b4c8313a2f751c3688a
Mubin Ansari <mubinansari910@gmail.com>
2023-03-12 02:09:53 +0530

    update UI of ChatMesage and ChatRoom components

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import ChatMessage from './ChatMessage';
 
 export default function ChatRoom() {
-  return <div>ChatRoom</div>;
+  return (
+    <div className='chat-room'>
+      <ChatMessage
+        text={
+          'Hi  dsfjasdfkljsdajfkjakldsjfkjadklsfjlkajsdklfjlkadsjlkfjalksjdlfkjalksdjflkjlkdfdafslksdjfjdasjfjasdkjfkjsdkfkjdksjfkjdkasjfkjadksjfkjasdkjfkjaskdjfkjdsafjlsd;jfkjsd;akfjkjasdkfjkjsdkfjsjdafkadsj'
+        }
+        uid={'8A0gaQKLUMPPG6kHsPPA2tcuM6G2'}
+        photoURL={
+          'https://lh3.googleusercontent.com/a/AGNmyxaF3eWvgrvE2cYSIoAKoBSuKDawdXk7iBdKjSNJ=s96-c'
+        }
+      />
+      <ChatMessage
+        text={'Meow'}
+        uid={'8A0gaQKLUMPPdG6kHsPPA2tcuM6G2'}
+        photoURL={
+          'https://lh3.googleusercontent.com/a/AGNmyxaF3eWvgrvE2cYSIoAKoBSuKDawdXk7iBdKjSNJ=s96-c'
+        }
+      />
+    </div>
+  );
 }
